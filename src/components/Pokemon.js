5e59e2af27ea7d9db8a14f146c923567af07b8dd
Julian Hoyos <jhoyos135@gmail.com>
2019-04-19 14:58:22 -0400

    changed images url

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const Pokemon = (props) => {
+
+  const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/';
+
+  const padToThree = (num) => {
+    return num <= 999 ? `00${num}`.slice(-3) : num
+  }
   
-    const { pokemon } = props;
+  const { pokemon } = props;
+  let imgSrc = `${POKE_API}${padToThree(pokemon.id)}.png`
 
     return (
       <div className="single_poke">
-        <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt=""/>
+        <img src={imgSrc} alt=""/>
         <p className="poke_name">{pokemon.name}</p>
       </div>
     )
